5145c1ea87b2f329e2f07e09c7b4d319b1382bee
WuCoding <wcy_best@outlook.com>
2019-12-13 03:18:28 +0800

    ...

--- testFinal.cc
+++ testFinal.cc
@@ -1,34 +1,59 @@
 #include "func.h"
 
+void print(XMLElement* elem,string elemName){
+	const char* txt;
+	if(elem==nullptr){
+		cout<<elemName<<" is null"<<endl;
+	}else{
+		txt=elem->GetText();
+		if(txt==nullptr){
+			cout<<elemName<<" :null"<<endl;
+		}else{
+			cout<<elemName<<": "<<txt<<endl;
+		}
+	}
+}
+
 void test(string fileName){
 	XMLDocument doc;
 	XMLError errXml=doc.LoadFile(fileName.c_str());
 	if(XML_SUCCESS==errXml){
-		const char* txt;
 		XMLElement* Rss=doc.RootElement();//rss
 		XMLElement* Channel=Rss->FirstChildElement("channel");//channel
-		//------------------------------------------------------------------------
+
 		XMLElement* Title=Channel->FirstChildElement("title");//title
 		XMLElement* Link=Channel->FirstChildElement("link");//link
 		XMLElement* Description=Channel->FirstChildElement("description");//description
+		XMLElement* Content=Channel->FirstChildElement("content");//content
+		
+		print(Title,"title");
+		print(Link,"link");
+		print(Description,"description");
+		print(Content,"content");
+
+		
+#if 0
 		txt=Title->GetText();
 		if(txt==nullptr){
 			cout<<"title: nullptr"<<endl;
 		}else{
 			cout<<"title: "<< txt <<endl;
 		}
+
 		txt=Link->GetText();
 		if(txt==nullptr){
 			cout<<"link: null"<<endl;
 		}else{
 			cout<<"link: "<< txt <<endl;
 		}
-		
-		if(Description->GetText()==nullptr){
+
+		txt=Description->GetText();	
+		if(txt==nullptr){
 			cout<<"description: null"<<endl;
 		}else{
-			cout<<"description: "<<Description->GetText() <<endl;
+			cout<<"description: "<< txt <<endl;
 		}
+#endif
 
 		//XMLElement* elmtItem=elmtChannel->LastChildElement("item");//item
 		//XMLElement* elmtAuthor=elmtItem->FirstChildElement();//author
@@ -37,13 +62,36 @@ void test(string fileName){
 		string s;
 		XMLElement* Item=Channel->FirstChildElement("item");//item
 		while(Item){
-			cin>>s;
 			Title=Item->FirstChildElement("title");
 			Link=Item->FirstChildElement("link");
 			Description=Item->FirstChildElement("description");
-			cout<<"title: "<< Title->GetText() <<endl;
-			cout<<"link: "<< Link->GetText() <<endl;
-			cout<<"description: "<<Description->GetText()<<endl;
+			Content=Item->FirstChildElement("content");
+
+			cin>>s;
+			print(Title,"title");
+			cin>>s;
+			print(Link,"link");
+			cin>>s;
+			print(Description,"description");
+			cin>>s;
+			print(Content,"content");
+#if 0
+			if(txt==nullptr){
+				cout<<"link: null"<<endl;
+			}else{
+				cout<<"link: "<< txt <<endl;
+			}
+
+			txt=Description->GetText();	
+			if(txt==nullptr){
+				cout<<"description: null"<<endl;
+			}else{
+				cout<<"description: "<< txt <<endl;
+			}
+#endif
+			//cout<<"title: "<< Title->GetText() <<endl;
+			//cout<<"link: "<< Link->GetText() <<endl;
+			//cout<<"description: "<<Description->GetText()<<endl;
 			Item=Item->NextSiblingElement("item");
 		}
 		//const char* pTitle=elmtTitle->GetText();
